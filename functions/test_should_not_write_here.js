4652b57427596ebd894525c2b32e347403db2df0
kameshdangeti <kameshdangeti@gmail.com>
2024-03-09 17:05:52 +0530

    the folder for emulators code has been commited

---
+++ functions/test_should_not_write_here.js
@@ -0,0 +1,42 @@
+const admin = require("firebase-admin");
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter, connectFirestoreEmulator } = require('firebase-admin/firestore');
+
+async function fun() {
+    var serviceAccount = require("./myproject-a1ddc-firebase-adminsdk-86ld4-c7a949f6be.json");
+    initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+    let db = null;
+    db = getFirestore();
+    const docRef = db.collection('users').doc('alovelace');
+
+    const aTuringRef = db.collection('users').doc('aturing');
+    try {
+        await docRef.set({
+            first: 'Ada',
+            last: 'Lovelace',
+            born: 1815
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+    aTuringRef.set({
+        first: 'Alan',
+        middle: 'Mathison',
+        last: 'Turing',
+          born: 1912
+    }, { merge: true }).then(() => { console.log('insert completed') });
+    const snapshot = await docRef.get();
+  let born = snapshot.data().born;
+
+  while (true) {
+    born = born + 1;
+    await docRef.update({ born });
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    };
+};
+
+fun().then(() => { console.log('completed') });
+
